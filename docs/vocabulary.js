5601cf4d5d505761031d913eb1ef943dc545030d
lijiawei <jawoo@qq.com>
2022-02-12 13:10:16 +0800

    lint

--- docs/vocabulary.js
+++ docs/vocabulary.js
@@ -1,6 +1,28 @@
-window.vocabulary = ["able", "about", "absorb", "absent", "academic"];
+window.vocabulary = [
+  'able',
+  'about',
+  'absorb',
+  'absent',
+  'academic',
+  'accent',
+  'accident',
+  'according',
+  'account',
+  'accurate',
+  'accuse',
+  'ache',
+  'achieve',
+  'achievement',
+  'act',
+  'action',
+  'activity',
+  'actor',
+  'actress',
+  'actual',
+];
+
 function vueDictionToday() {
-  var component = vueDictionExtend({
+  const component = vueDictionExtend({
     word: vocabulary[Math.floor(new Date().valueOf() / (1000 * 60 * 60 * 24)) % vocabulary.length],
     autoLoad: true,
   });
