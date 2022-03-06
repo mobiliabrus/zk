33ca7f5b844a66d9f97ac1055f1ac1fcb70e29b0
lijiawei <jawoo@qq.com>
2022-03-06 21:51:00 +0800

    style

--- docs/components/badge/index.js
+++ docs/components/badge/index.js
@@ -3,10 +3,14 @@ const component = {
     title: {
       type: String,
     },
+    inline: {
+      type: Boolean,
+      default: false
+    },
   },
 };
 
 component.template =
-  '<div class="badge"><div v-if="title" class="badge-title">{{ title }}</div><div class="badge-content"><slot></slot></div></div>';
+  '<span class="badge" :style="{ display: inline ? \'inline-block\' : \'block\' }"><span v-if="title" class="badge-title">{{ title }}</span><span class="badge-content"><slot></slot></span></span>';
 
 window.vueBadge = component;
