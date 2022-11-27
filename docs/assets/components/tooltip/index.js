ec92594b7404a6e122eb3a82268525f31d6ffa4a
chiaweilee <97830321+mobiliabrus@users.noreply.github.com>
2022-11-27 16:05:11 +0800

    docs

--- docs/assets/components/tooltip/index.js
+++
@@ -1,8 +0,0 @@
-window.vueTooltip = {
-  template: '<span class="tooltip"><span class="tooltip-inner">{{title}}</span><slot></slot></span>',
-  props: {
-    title: {
-      type: String,
-    },
-  },
-};
